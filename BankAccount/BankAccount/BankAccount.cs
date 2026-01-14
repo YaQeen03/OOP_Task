58d8d3915feedf3d7432e8a27f16b60ef3fb522d
YaQeen03 <yfaisa1323@gmail.com>
2026-01-14 03:24:00 +0300

    Add new file

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,117 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace ConsoleApp1
+{
+    internal class BankAccount
+    {
+        class BankAccount
+        {
+
+            private int AccountNumber;
+            private string AccountHolderName;
+            private double balance;
+            private string AccountType;
+            private string BranchName;
+
+            public BankAccount(int AccountNumber, string AccountHolderName, double balance, string AccountType, string BranchName)
+            {
+                this.AccountNumber = AccountNumber;
+                this.AccountHolderName = AccountHolderName;
+                this.balance = balance;
+                this.AccountType = AccountType;
+                this.BranchName = BranchName;
+            }
+            ~BankAccount()
+            {
+                Console.WriteLine("object destroyed.");
+            }
+
+            public int Account_Number
+            {
+                get { return AccountNumber; }
+                set { AccountNumber = value; }
+            }
+            public string Account_HolderName
+            {
+                get { return AccountHolderName; }
+                set { AccountHolderName = value; }
+
+            }
+            public double Balance
+            {
+                set
+                {
+                    if (value >= 0)
+                        balance = value;
+                    else
+                        Console.WriteLine("Error: Balance cannot be negative.");
+                }
+                get { return balance; }
+            }
+            public string Account_Type
+            {
+                get { return AccountType; }
+                set { AccountType = value; }
+
+            }
+            public string Branch_Name
+            {
+                set { BranchName = value; }
+                get { return BranchName; }
+
+            }
+            public void Deposit(double x)
+            {
+                balance += x;
+
+
+            }
+            public void Withdraw(double w)
+            {
+                if (w <= balance && w % 5 == 0)
+                {
+                    balance -= w;
+                    Console.WriteLine("The withdrawal was successful.");
+                }
+                else if (w <= Balance && w % 5 != 0)
+                {
+
+                    Console.WriteLine("Error: The requested amount cannot be withdrawn as it does not match the available denominations.");
+
+                }
+                else
+                {
+                    Console.WriteLine(" Error:Insufficient balance");
+                }
+
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine("Account Number: " + AccountNumber);
+                Console.WriteLine("Account Holder: " + AccountHolderName);
+                Console.WriteLine("Balance: " + balance.ToString("F2"));
+                Console.WriteLine("Account Type: " + AccountType);
+                Console.WriteLine("Branch Name: " + BranchName);
+            }
+            static void Main(string[] args)
+            {
+                BankAccount account1 = new BankAccount(210, "Yaqeen", 2000, "Salary Account", "Irbid");
+                BankAccount account2 = new BankAccount(219, "Faisal", 3000, "Saving Account", "Amman");
+
+                account1.Deposit(50.08);
+                account1.Withdraw(1800);
+                account1.DisplayInfo();
+
+                account2.Deposit(77.98);
+                account2.Withdraw(3090);
+                account2.DisplayInfo();
+
+            }
+        }
+    }
+}
+
+
+
+
---
+++ car/car/CarTask.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class CarTask
+    {
+
+        private string Brand;
+        private string Model;
+        private int Year;
+
+        public CarTask(string brand, string model, int year)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+        }
+
+
+        public void Start()
+        {
+            Console.WriteLine(Brand + Model + " is starting.");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine(Brand + Model + " is driving.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine(Brand + Model + "has stopped.");
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Brand: " + Brand + "  " + "Model: " + Model + " Year: " + Year);
+        }
+    }
+
+    class CarTask
+    {
+        static void Main(string[] args)
+        {
+
+            CarTask car1 = new CarTask("Mercedes", "C-Class", 2022);
+            CarTask car2 = new CarTask("BMW", "X5", 2022);
+
+
+            car1.Start();
+            car1.Drive();
+            car1.Stop();
+            car1.DisplayInfo();
+
+            Console.WriteLine();
+
+            car2.Start();
+            car2.Drive();
+            car2.Stop();
+            car2.DisplayInfo();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
